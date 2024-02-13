714cb6c16ff424c00a2527367df08e974e93990f
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-02-13 21:19:56 +0700

    enable user to add text-based input to run the singleton program

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 import { run as runSingleton } from "./singleton";
+import * as readline from "node:readline";
 
-runSingleton();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Type something: ", answer => {
+  switch (answer) {
+    case "singleton":
+      runSingleton();
+      break;
+
+    default:
+      break;
+  }
+
+  rl.close();
+})
+
+// runSingleton();
